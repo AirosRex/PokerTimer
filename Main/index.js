8ced84e33ac246d93dd0da48dad543790f0ef15b
Alex Soria <alex.e.soria@gmail.com>
2024-08-23 16:34:01 -0700

    Implementing drop down elements

--- Main/index.js
+++ Main/index.js
@@ -1,1 +1,13 @@
-//This is a placeholder until I need JS
+document.getElementById('selectTimer').addEventListener('change', function() {
+    var selectedValue = this.value;
+    document.getElementById('selectedTimer').innerText = "You selected: " + selectedValue;
+});
+
+document.getElementById('selectBreaks').addEventListener('change', function() {
+    var selectedValue = this.value;
+    if (selectedValue == "No breaks") {
+        document.getElementById('selectedBreaks').innerText = "There will be no breaks";
+    } else {
+        document.getElementById('selectedBreaks').innerText = "There will be a break every " + selectedValue;
+    }
+});
